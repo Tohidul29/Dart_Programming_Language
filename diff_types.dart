4b5e0ab2515b921cf50a12e07e02aa29c23589ed
Tohidul29 <tohidswfu@gmail.com>
2023-07-06 22:35:31 +0600

    first commit

---
+++ diff_types.dart
@@ -0,0 +1,24 @@
+void main(){
+
+  //int:
+  int age = 25;
+
+  //double:
+  double temp = 37.33;
+
+  //boolean:
+  bool isMarried = false;
+
+  //var:
+  var aboutMe = 'I am Tohidul Islam and my current age is: $age. My marriage status: $isMarried';
+
+  //string:
+  String myEducation = 'Bachelor: BSc in Computer Science and Technology';
+
+  //print all the statement:
+  print(age);
+  print(temp);
+  print(isMarried);
+  print(aboutMe);
+  print(myEducation);
+}
