907aeb1b666e3eb42848333610d773062f4682e2
Tohidul29 <tohidswfu@gmail.com>
2023-07-06 22:39:48 +0600

    about variable(var)

---
+++ Var.dart
@@ -0,0 +1,18 @@
+void main(){
+
+  //using var in different way:
+  var name = 'MD TOHIDUL ISLAM';
+  var age = 25;
+  var height = 6.0;
+  var currentStatus = 'Student';
+  var isMarried = false;
+  var loveCoding = true;
+
+  //print all statements:
+  print(name);
+  print(age);
+  print(height);
+  print(currentStatus);
+  print(isMarried);
+  print(loveCoding);
+}
