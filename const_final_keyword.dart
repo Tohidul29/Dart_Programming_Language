c89112e43a44a751cb732249495113a5a0eeea93
Tohidul29 <tohidswfu@gmail.com>
2023-07-07 22:46:45 +0600

    added

---
+++ const_final_keyword.dart
@@ -0,0 +1,22 @@
+void main(){
+
+  //const:
+  const daysInWeek = 7;
+  const daysInYear = 365;
+  const daysInMonth = 30;
+  const daysInFebruary = 28;
+  const PI = 3.1416;
+
+  //final:
+  final countryName = 'Bangladesh';
+  final favoritePlayer = 'Tamim Iqbal';
+
+  print('One week: ${daysInWeek} days');
+  print('One year: ${daysInYear} days');
+  print('One month: ${daysInMonth} days');
+  print('February: ${daysInFebruary} days');
+  print('Value of PI: ${PI}');
+  print('My country: ${countryName}');
+  print('My favorite player: ${favoritePlayer}');
+
+}
