2f3a1c6be31e1dd2e9b20f20f6c27d0c7b601593
Tohidul29 <tohidswfu@gmail.com>
2023-07-07 22:30:45 +0600

    added

---
+++ String_Literals.dart
@@ -0,0 +1,16 @@
+void main() {
+  String name = 'MD TOHIDUL ISLAM';
+  String s2 = "MD TOHIDUL ISLAM";
+  String s3 = 'It\'s raining';
+  String s4 = "It's raining";
+
+  String s5 = 'My name is $name';
+
+  print(name);
+  print(s2);
+  print(s3);
+  print(s4);
+  print(s5);
+  print('The total character of my name is: ' + name.length.toString());
+
+}
---
+++ mathematical_term.dart
@@ -0,0 +1,14 @@
+void main(){
+
+  //declare variable:
+  int a = 22;
+  int b = 10;
+
+  //print mathematical statement:
+  print('The sum of a and b is: ${a+b}');
+  print('The difference between a and b is: ${a-b}');
+  print('The multiplication of a and b is: ${a*b}');
+  print('The division of a and b is: ${a/b}');
+  print('The reminder of a and b is: ${a%b}');
+
+}
